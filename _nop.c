7c297aa37573d39518bd92d4734fc27c1713d4be
AllenKeelig <alallen.keeling@atlasschool.com>
2024-04-19 08:56:12 -0500

    monty

--- _nop.c
+++ _nop.c
@@ -4,9 +4,9 @@
 /**
  * _nop - Does nothing
  *
- * This function serves as a no-operation (NOP) command for the Monty interpreter.
+ * This function serves as a no-operation (NOP) command for the Monty
  * It takes in a stack and line number, but performs no action on them.
- * It is used when there is no operation associated with a particular instruction.
+ * It is used when there is no operation associated with a particular
  *
  * @stack: A double pointer to the top of the stack (linked list).
  * @line_number: The current line number in the script being interpreted.
