0169436ded58d2d3ad002479dadff743e7a4cbd8
AllenKeelig <alallen.keeling@atlasschool.com>
2024-04-19 07:02:44 -0500

    monty

--- _operation.c
+++ _operation.c
@@ -18,18 +18,7 @@ void (*go(char *op_f, unsigned int l, stack_t **stak))(stack_t**, unsigned int)
 	instruction_t operation[] = {
 		{"pall", _pall},
 		{"add", _add},
-		{"pint", _pint},
-		{"swap", _swap},
-		{"pop", _pop},
-		{"sub", _sub},
-		{"div", _div},
-		{"mul", _mul},
-		{"mod", _mod},
 		{"nop", _nop},
-		{"rotl", _rotl},
-		{"rotr", _rotr},
-		{"pchar", _pchar},
-		{"pstr", _pstr},
 		{NULL, NULL}
 	};
 
